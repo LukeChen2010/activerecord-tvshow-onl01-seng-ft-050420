3470bfa4696df0d914dce8b7c219167a22a4a400
Luke Chen <luke.chen.2010@gmail.com>
2020-05-26 21:52:37 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,36 @@
+class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+    show = Show.maximum(:rating)
+    return show
+  end
+  
+  def self.most_popular_show
+    show = Show.where(rating: Show.highest_rating)[0]
+    return show
+  end
+  
+  def self.lowest_rating
+    show = Show.minimum(:rating)
+    return show
+  end
+
+  def self.least_popular_show
+    show = Show.where(rating: Show.lowest_rating)[0]
+    return show
+  end
+  
+  def self.ratings_sum
+    return Show.sum(:rating)
+  end
+  
+  def self.popular_shows
+    shows = Show.where("rating > ?", 5)
+    return shows
+  end
+  
+  def self.shows_by_alphabetical_order
+    shows = Show.order(:name)
+    return shows
+  end
+end
