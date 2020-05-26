adf89226b23da2ba72c21427552a7b9a6118a609
Luke Chen <luke.chen.2010@gmail.com>
2020-05-26 21:39:49 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,12 @@
 class CreateShows < ActiveRecord::Migrate[5.2]
-  
-  create_table shows do |t|
-    t.string :name
-    t.string :network
-    t.string :day
-    t.integer :rating
+  def change
+    create_table shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
   end
+  
   
 end
